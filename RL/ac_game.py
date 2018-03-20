8b09266e7640d47cc7ed97b72c3fc3acf9654c0b
zhaoguanjun <773216251@qq.com>
2018-03-20 13:51:14 +0800

    actor-critic, reinforcement learning algorithm

---
+++ RL/ac_game.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+import numpy as np
+import gym
+import sys
+
+sys.path.append('./')
+from actor_critic import Actor, Critic
+
+# pylint: disable=protected-access
+
+np.random.seed(2)
+tf.set_random_seed(2)
+
+MAX_EPISODE = 3000
+MAX_EP_STEPS = 1000
+DISPLAY_REWARD_THRESHOLD = 200
+LR_A = 0.001
+LR_C = 0.01
+
+
+class Game(object):
+    def __init__(self, env, actor, critic):
+        self.env = env
+        self.actor = actor
+        self.critic = critic
+
+    def run(self):
+        RENDER = False
+        for i_episode in range(MAX_EPISODE):
+            s = self.env.reset()
+            t = 0
+            track_r = []
+            while True:
+                if RENDER:
+                    self.env.render()
+
+                a = self.actor.choose_action(s)
+                s_, r, done, info = self.env.step(a)
+
+                if done:
+                    r = -20
+
+                track_r.append(r)
+
+                self.td_error = self.critic.learn(s, r, s_)
+                self.actor.learn(s, a, self.td_error)
+
+                s = s_
+                t += 1
+
+                if done or t > MAX_EP_STEPS:
+                    ep_rs_sum = sum(track_r)
+
+                    if not hasattr(self, 'reward'):
+                        self.reward = ep_rs_sum
+                    else:
+                        self.reward = self.reward * 0.95 + ep_rs_sum * 0.05
+
+                    if self.reward > DISPLAY_REWARD_THRESHOLD:
+                        RENDER = True
+                    print('episode: ', i_episode, 'reward: ', int(self.reward))
+                    break
+
+
+def CartPoleAC():
+    env = gym.make('CartPole-v0')
+    env.seed(1)
+    env = env.unwrapped
+    env.reset()
+
+    n_features = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
+    sess = tf.Session()
+
+    actor = Actor(sess, n_features, n_actions, lr=LR_A)
+    critic = Critic(sess, n_features, lr=LR_C)
+    sess.run(tf.global_variables_initializer())
+
+    game = Game(env, actor, critic)
+    game.run()
+
+
+if __name__ == '__main__':
+    CartPoleAC()
---
+++ RL/actor_critic.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import numpy as np
+
+np.random.seed(2)
+tf.set_random_seed(2)
+
+GAMMA = 0.9
+
+
+class Actor(object):
+    def __init__(self, sess, n_features, n_actions, lr=0.001):
+        self.sess = sess
+        self.s = tf.placeholder(tf.float32, [1, n_features], 'state')
+        self.a = tf.placeholder(tf.int32, None, 'act')
+        self.td_error = tf.placeholder(tf.float32, None, 'td_error')
+
+        with tf.variable_scope('Actor'):
+            l1 = tf.layers.dense(inputs=self.s,
+                                 units=20,
+                                 activation=tf.nn.relu,
+                                 kernel_initializer=tf.random_normal_initializer(0.0, 0.1),
+                                 bias_initializer=tf.constant_initializer(0.1),
+                                 name='l1')
+
+            self.acts_prob = tf.layers.dense(inputs=l1,
+                                             units=n_actions,
+                                             activation=tf.nn.softmax,
+                                             kernel_initializer=tf.random_normal_initializer(0.0, 0.1),
+                                             bias_initializer=tf.constant_initializer(0.1),
+                                             name='acts_prob')
+
+        with tf.variable_scope('exp_v'):
+            log_prob = tf.log(self.acts_prob[0, self.a])
+            self.exp_v = tf.reduce_mean(log_prob * self.td_error)
+
+        with tf.variable_scope('train'):
+            self.train_op = tf.train.AdamOptimizer(lr).minimize(-self.exp_v)
+
+    def learn(self, s, a, td):
+        s = s[np.newaxis, :]
+        feed_dict = {self.s: s, self.a: a, self.td_error: td}
+        _, exp_v = self.sess.run([self.train_op, self.exp_v], feed_dict)
+        return exp_v
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]
+        probs = self.sess.run(self.acts_prob, {self.s: s})
+        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())
+
+
+class Critic(object):
+    def __init__(self, sess, n_features, lr=0.001):
+        self.sess = sess
+        self.s = tf.placeholder(tf.float32, [1, n_features], name='state')
+        self.v_ = tf.placeholder(tf.float32, [1, 1], name='v_next')
+        self.r = tf.placeholder(tf.float32, None, name='r')
+
+        with tf.variable_scope('Critic'):
+            l1 = tf.layers.dense(inputs=self.s,
+                                 units=20,
+                                 activation=tf.nn.relu,
+                                 kernel_initializer=tf.random_normal_initializer(0., .1),
+                                 bias_initializer=tf.constant_initializer(.1),
+                                 name='l1')
+
+            self.v = tf.layers.dense(inputs=l1,
+                                     units=1,
+                                     activation=None,
+                                     kernel_initializer=tf.random_normal_initializer(0., .1),
+                                     bias_initializer=tf.constant_initializer(.1),
+                                     name='v')
+
+        with tf.variable_scope('squared_TD_error'):
+            self.td_error = self.r + GAMMA * self.v_ - self.v
+            self.loss = tf.square(self.td_error)
+
+        with tf.variable_scope('train'):
+            self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
+
+    def learn(self, s, r, s_):
+        s, s_ = s[np.newaxis, :], s_[np.newaxis, :]
+
+        v_ = self.sess.run(self.v, {self.s : s_})
+        td_error, _ = self.sess.run([self.td_error, self.train_op], {self.s: s, self.v_: v_, self.r: r})
+
+        return td_error
