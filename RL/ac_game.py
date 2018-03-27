cd39ad0b43a780d2cab88d02977d6c234d901545
zhaoguanjun <773216251@qq.com>
2018-03-27 16:45:15 +0800

    add mountain car, hard to covergence

--- RL/ac_game.py
+++ RL/ac_game.py
@@ -122,6 +122,50 @@ class Game(object):
         plt.ylabel('reward')
         plt.show()
 
+    def run_mountain_car(self):
+        RENDER = True
+        for i_episode in range(MAX_EPISODE):
+            s = self.env.reset()
+            t = 0
+            track_r = []
+            while True:
+                # if RENDER:
+                self.env.render()
+
+                a = self.actor.choose_action(s)
+                s_, r, done, info = self.env.step(a)
+
+                if done:
+                    r = 0
+
+                track_r.append(r)
+
+                self.td_error = self.critic.learn(s, r, s_)
+                self.actor.learn(s, a, self.td_error)
+
+                s = s_
+                t += 1
+
+                if done or t > MAX_EP_STEPS:
+                    ep_rs_sum = sum(track_r)
+                    self.reward_his.append(ep_rs_sum)
+
+                    if not hasattr(self, 'reward'):
+                        self.reward = ep_rs_sum
+                    else:
+                        self.reward = self.reward * 0.95 + ep_rs_sum * 0.05
+
+                    if r > DISPLAY_REWARD_THRESHOLD:
+                        RENDER = True
+                    print('episode: ', i_episode, 'reward: ', int(self.reward))
+                    break
+
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(len(self.reward_his)), self.reward_his)
+        plt.xlabel('episode')
+        plt.ylabel('reward')
+        plt.show()
+
 
 def CartPoleAC():
     env1 = gym.make('CartPole-v0')
@@ -149,5 +193,25 @@ def CartPoleAC():
     # game.run_double()
 
 
+def MountainCar():
+    env = gym.make('MountainCar-v0')
+    env = env.unwrapped
+    env.reset()
+    env.render()
+
+    n_features = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
+    sess = tf.Session()
+
+    actor = Actor(sess, n_features, n_actions, lr=LR_A)
+    critic = Critic(sess, n_features, lr=LR_C)
+    sess.run(tf.global_variables_initializer())
+
+    game = Game(env, actor, critic)
+    game.run_mountain_car()
+
+
 if __name__ == '__main__':
-    CartPoleAC()
+    # CartPoleAC()
+    MountainCar()
