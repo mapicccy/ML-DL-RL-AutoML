ecb66c2b6169c8298678cfd5889e005bc6c2f08d
zhaoguanjun <773216251@qq.com>
2018-04-02 18:32:58 +0800

    for test

--- RL/run_mountain_car_on_dqn.py
+++ RL/run_mountain_car_on_dqn.py
@@ -9,7 +9,7 @@ from memory import Memory
 
 MEMORY_CAPACITY = 3000
 BATCH_SIZE = 32
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.001
 GAMMA = 0.9
 NUM_EPISODE = 2000
 
@@ -22,45 +22,100 @@ n_features = env.state.shape[0]
 print('actions=', n_actions, 'n_features=', n_features)
 
 memory0 = Memory(n_features, MEMORY_CAPACITY, n_features * 2 + 2, BATCH_SIZE)
+memory1 = Memory(n_features, MEMORY_CAPACITY, n_features * 2 + 2, BATCH_SIZE)
 
-sess = tf.Session()
+sess0 = tf.Session()
+sess1 = tf.Session()
 
-dqn = DDQN(n_actions,
-           n_features,
-           memory0,
-           name='ddqn0',
-           learning_rate=LEARNING_RATE,
-           reward_delay=GAMMA,
-           replace_target_iter=200,
-           double_q=False)
+dqn = [DDQN(n_actions,
+            n_features,
+            memory0,
+            name='dqn0',
+            learning_rate=LEARNING_RATE,
+            reward_delay=GAMMA,
+            replace_target_iter=200,
+            double_q=False,
+            sess=sess0),
+       DDQN(n_actions,
+            n_features,
+            memory1,
+            name='dqn1',
+            learning_rate=LEARNING_RATE,
+            reward_delay=GAMMA,
+            replace_target_iter=200,
+            double_q=False,
+            sess=sess1)]
+
+sess0.run(tf.global_variables_initializer())
+sess1.run(tf.global_variables_initializer())
 
 episode_reward = []
-for i in range(NUM_EPISODE):
-    sum_reward = 0
+episode_position = []
+episode_negative = []
+total_step = [0, 0]
+for episode in range(NUM_EPISODE):
+    for i in range(len(dqn)):
+        sum_reward = 0
+        step = 0
+        state = env.reset()
+        while True:
+            step += 1
+            total_step[i] += 1
+
+            # regular way for position and negative network
+            # action = dqn[i].choose_action(state)
+
+            # another way
+            action_value = dqn[i].action_value(state)
+            action = np.argmax(action_value) if i == 0 else np.argmin(action_value)
+            if np.random.uniform() > dqn[i].epsilon:
+                action = np.random.randint(0, n_actions)
+
+            state_, reward, done, _ = env.step(action)
+
+            # this condition considering the velocity
+            # if done:
+            #     reward = 30
+            # else:
+            #     reward = abs(state[0] - state_[0])
+
+            if done:
+                reward = 30
+
+            new_reward = reward if i == 0 else -reward
+            dqn[i].store_transition(state, action, new_reward, state_)
+
+            # this condition is to get a good result relatively
+            # if step > 200 and step % 5 == 0:
+            #     dqn[i].learn()
+
+            # this condition is to get the best result
+            if (total_step[i] > 200) and step % 5 == 0:
+                dqn[i].learn()
+
+            sum_reward += reward
+            state = state_
+
+            if done or step > 1999:
+                print(dqn[i].name, episode, step)
+                if i == 0:
+                    episode_position.append(step)
+                else:
+                    episode_negative.append(step)
+                dqn[i].set_replace_target_iter(5 * step)
+                break
+
     step = 0
+    sum_reward = 0
     state = env.reset()
     while True:
+        env.render()
         step += 1
-        action = dqn.choose_action(state)
-        state_, reward, done, _ = env.step(action)
+        action_value = np.ones(n_actions)
+        # TODO ...
 
-        if done:
-            reward = 30
-
-        dqn.store_transition(state, action, reward, state_)
-
-        if step > 200 and step % 5 == 0:
-            dqn.learn()
-
-        sum_reward += reward
-        state = state_
-
-        if done or step > 2000:
-            print(dqn.name, i, step)
-            episode_reward.append(step)
-            # dqn.set_replace_iter(step * 5)
-            break
 
 import matplotlib.pyplot as plt
-plt.scatter(np.arange(len(episode_reward)), episode_reward, s=7, marker='o')
+plt.scatter(np.arange(len(episode_position)), episode_position, s=7, marker='o')
+plt.scatter(np.arange(len(episode_negative)), episode_negative, s=7, marker='o')
 plt.show()
