6b51d42e1ccddbdaaddee4171fcba7284a6719bf
zhaoguanjun <773216251@qq.com>
2018-10-15 23:02:28 +0800

    vgg16

--- DL/tensorflow/nets/vgg16.py
+++ DL/tensorflow/nets/vgg16.py
@@ -172,4 +172,12 @@ if __name__ == '__main__':
     vgg = VGG16(images, '../weights_file/', sess)
 
     from scipy.misc import imread, imresize
-    img1 =
+    img1 = imread('../images/laska.png', mode='RGB')
+    img1 = imresize(img1, (224, 224))
+
+    prob = sess.run(vgg.probs, feed_dict={vgg.images: img1})[0]
+    preds = (np.argsort(prob)[::-1])[0:5]
+
+    from imagenet_classes import class_names
+    for p in preds:
+        print(class_names[p], prob[p])
